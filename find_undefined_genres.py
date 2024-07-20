3af30de194d777c8e64a20fba7f3e18a85e63745
Thiemo Ottersbach <thiemo.ott@gmail.com>
2024-07-21 00:00:07 +0200

    chore: small fixes

--- find_undefined_genres.py
+++ find_undefined_genres.py
@@ -1,5 +1,6 @@
 import glob
 
+from colorama import just_fix_windows_console
 from mutagen.easyid3 import EasyID3
 from mutagen.flac import FLAC
 from mutagen.mp4 import MP4
@@ -10,51 +11,56 @@ FILETYPES = set(['flac', 'm4a', 'mp3', 'ogg', 'wav'])
 GENRES = set(
     [genre.strip() for genre in
      'Future House, Modern House, Dubstep, House, Progressive, Colour House'
-     .split(',')]
+     .replace('\r', '').replace('\n', '').split(',')]
 )
 
 
 def check_filetype(files):
     for filepath in files:
 
-        match filetype:
-            case 'flac':
-                audio = FLAC(filepath)
-                genre_fieldname = 'genre'
-            case 'mp3':
-                audio = EasyID3(filepath)
-                genre_fieldname = 'genre'
-            case 'm4a':
-                audio = MP4(filepath)
-                genre_fieldname = '©gen'
-            case 'ogg':
-                audio = OggVorbis(filepath)
-                genre_fieldname = 'genre'
-            case 'wav':
-                audio = WAVE(filepath)
-                genre_fieldname = 'TCON'
-            case _:
-                print(f'\033[93mUndefined filetype \"{filetype}\"\033[0m\n')
-                return
+        try:
+            match filetype:
+                case 'flac':
+                    audio = FLAC(filepath)
+                    genre_fieldname = 'genre'
+                case 'mp3':
+                    audio = EasyID3(filepath)
+                    genre_fieldname = 'genre'
+                case 'm4a':
+                    audio = MP4(filepath)
+                    genre_fieldname = '©gen'
+                case 'ogg':
+                    audio = OggVorbis(filepath)
+                    genre_fieldname = 'genre'
+                case 'wav':
+                    audio = WAVE(filepath)
+                    genre_fieldname = 'TCON'
+                case _:
+                    print(f'\033[93mUndefined filetype \"{filetype}\"\033[0m\n')
+                    return
 
-        if (genre_fieldname in audio.keys()):
-            genre_fields = audio[genre_fieldname]
+            if (genre_fieldname in audio.keys()):
+                genre_fields = audio[genre_fieldname]
 
-            # join duplicate fields (Will not work with m4a files)
-            genrestring = ','.join(genre_fields)
+                # join duplicate fields (Will not work with m4a files)
+                genrestring = ','.join(genre_fields)
 
-            # split genres
-            genres = [genre.strip() for genre in genrestring.split(',')]
+                # split genres
+                genres = [genre.strip() for genre in genrestring.split(',')]
 
-            undefined_genres = list(set(genres) - GENRES)
+                undefined_genres = list(set(genres) - GENRES)
 
-            if (len(undefined_genres) != 0):
-                print(filepath)
-                print(f'Undefined Genres:{undefined_genres}\n')
+                if (len(undefined_genres) != 0):
+                    print(filepath)
+                    print(f'Undefined Genres:{undefined_genres}\n')
+        except Exception as e:
+            print(e)
 
 
 if __name__ == '__main__':
+    just_fix_windows_console()
 
     for filetype in FILETYPES:
+        print(f'\033[96mNow Checking {filetype} files.\033[0m')
         files = glob.glob(f'**/*.{filetype}', recursive=True)
         check_filetype(files)
--- find_undefined_genres_filtered.py
+++ find_undefined_genres_filtered.py
@@ -1,5 +1,6 @@
 import glob
 
+from colorama import just_fix_windows_console
 from mutagen.easyid3 import EasyID3
 from mutagen.flac import FLAC
 from mutagen.id3 import ID3
@@ -12,9 +13,9 @@ GENRES = set(
     [genre.strip() for genre in
      'BPMCHANGE,Omnigenre,Acid House,Disco House,French House,Ghetto House,Jackin House,Melodic House,OS Future House,Progressive House,Soulful House,Speed House,'
      ' Complextro,Disco,Electro Swing,Future Bounce,Future Funk,Hardcore,Hardstyle,Hoe Trappin,Jall,Jersey Club,Meme,Pop,Psytrance,Riddim,Tearout,Techno,Trance,Trap,Chillwave,Synthwave,'
-     'Bass House,Colour House,Deep House ,Future House,Modern House,Piano House,Lofi House,Tech House,Colour Bass,Dubstep ,Breakcore,DnBnB'
+     'Bass House,Colour House,Deep House ,Future House,Modern House,Piano House,Lofi House,Tech House,Colour Bass,Dubstep ,Breakcore,DnBnB,'
      'Liquid DnB,Melodic DnB,Garage, Normie, Techcore'
-     .split(',')]
+     .replace('\r', '').replace('\n', '').split(',')]
 )
 GROUPING = 'For Mixing'
 
@@ -24,72 +25,75 @@ def check_filetype(files):
     for filepath in files:
         grouping_field_exists_and_corrects = False
 
-        match filetype:
+        try:
+            match filetype:
+                case 'flac':
+                    audio = FLAC(filepath)
+                    genre_fieldname = 'genre'
+                    grouping_key_name = 'grouping'
+                    grouping_tag_fieldname = 'GROUPING'
 
-            case 'flac':
-                audio = FLAC(filepath)
-                genre_fieldname = 'genre'
-                grouping_key_name = 'grouping'
-                grouping_tag_fieldname = 'GROUPING'
+                    if (grouping_key_name in audio.keys()):
+                        grouping_refined = str(audio[grouping_tag_fieldname][0])
+                        if (grouping_refined == GROUPING):
+                            grouping_field_exists_and_corrects = True
+                case 'm4a':
+                    audio = MP4(filepath)
+                    genre_fieldname = '©gen'
+                    grouping_fieldname = '----:com.apple.iTunes:GROUPING'
+                    if (grouping_fieldname in audio.keys()):
+                        grouping_raw = str(audio['----:com.apple.iTunes:GROUPING'][0])
+                        grouping_refined = grouping_raw[2:len(grouping_raw) - 1]
 
-                if (grouping_key_name in audio.keys()):
-                    grouping_refined = str(audio[grouping_tag_fieldname][0])
-                    if (grouping_refined == GROUPING):
+                        if (grouping_refined == GROUPING):
+                            grouping_field_exists_and_corrects = True
+                case 'mp3':
+                    audio = EasyID3(filepath)
+                    genre_fieldname = 'genre'
+                    grouping_fieldname = 'GRP1'
+                    helper = ID3(filepath)
+                    if (grouping_fieldname in helper.keys() and helper[grouping_fieldname] == GROUPING):
                         grouping_field_exists_and_corrects = True
-            case 'm4a':
-                audio = MP4(filepath)
-                genre_fieldname = '©gen'
-                grouping_fieldname = '----:com.apple.iTunes:GROUPING'
-                if (grouping_fieldname in audio.keys()):
-                    grouping_raw = str(audio['----:com.apple.iTunes:GROUPING'][0])
-                    grouping_refined = grouping_raw[2:len(grouping_raw) - 1]
-
-                    if (grouping_refined == GROUPING):
+                case 'ogg':
+                    audio = OggVorbis(filepath)
+                    genre_fieldname = 'genre'
+                    grouping_fieldname = 'grouping'
+                    if (grouping_fieldname in audio.keys()):
+                        grouping_refined = str(audio[grouping_fieldname][0])
+                        if (grouping_refined == GROUPING):
+                            grouping_field_exists_and_corrects = True
+                case 'wav':
+                    audio = WAVE(filepath)
+                    genre_fieldname = 'TCON'
+                    grouping_fieldname = 'GRP1'
+                    if (grouping_fieldname in audio.keys() and audio[grouping_fieldname] == GROUPING):
                         grouping_field_exists_and_corrects = True
-            case 'mp3':
-                audio = EasyID3(filepath)
-                genre_fieldname = 'genre'
-                grouping_fieldname = 'GRP1'
-                helper = ID3(filepath)
-                if (grouping_fieldname in helper.keys() and helper[grouping_fieldname] == GROUPING):
-                    grouping_field_exists_and_corrects = True
-            case 'ogg':
-                audio = OggVorbis(filepath)
-                genre_fieldname = 'genre'
-                grouping_fieldname = 'grouping'
-                if (grouping_fieldname in audio.keys()):
-                    grouping_refined = str(audio[grouping_fieldname][0])
-                    if (grouping_refined == GROUPING):
-                        grouping_field_exists_and_corrects = True
-            case 'wav':
-                audio = WAVE(filepath)
-                genre_fieldname = 'TCON'
-                grouping_fieldname = 'GRP1'
-                if (grouping_fieldname in audio.keys() and audio[grouping_fieldname] == GROUPING):
-                    grouping_field_exists_and_corrects = True
-            case _:
-                print(f'\033[93mUndefined filetype \"{filetype}\"\033[0m\n')
-                return
+                case _:
+                    print(f'\033[93mUndefined filetype \"{filetype}\"\033[0m\n')
+                    return
 
-        if (genre_fieldname in audio.keys() and grouping_field_exists_and_corrects):
-            genre_fields = audio[genre_fieldname]
+            if (genre_fieldname in audio.keys() and grouping_field_exists_and_corrects):
+                genre_fields = audio[genre_fieldname]
 
-            # join duplicate fields (Will not work with m4a files)
-            genrestring = ','.join(genre_fields)
+                # join duplicate fields (Will not work with m4a files)
+                genrestring = ','.join(genre_fields)
 
-            # split genres
-            genres = [genre.strip() for genre in genrestring.split(',')]
+                # split genres
+                genres = [genre.strip() for genre in genrestring.split(',')]
 
-            undefined_genres = list(set(genres) - GENRES)
+                undefined_genres = list(set(genres) - GENRES)
 
-            if (len(undefined_genres) != 0):
-                print(filepath)
-                print(f'Undefined Genres:{undefined_genres}\n')
+                if (len(undefined_genres) != 0):
+                    print(filepath)
+                    print(f'Undefined Genres:{undefined_genres}\n')
+        except Exception as e:
+            print(e)
 
 
 if __name__ == '__main__':
+    just_fix_windows_console()
 
     for filetype in FILETYPES:
-        print (f'Now Checking {filetype} files.')
+        print(f'\033[96mNow Checking {filetype} files.\033[0m')
         files = glob.glob(f'**/*.{filetype}', recursive=True)
         check_filetype(files)
