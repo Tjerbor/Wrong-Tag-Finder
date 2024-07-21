b93bef737aa15b4f40673f66a0f9abdaee125a3b
Thiemo Ottersbach <thiemo.ott@gmail.com>
2024-07-21 12:50:52 +0200

    feat: Add aif(f) support for undefined tag finder

--- find_undefined_genres.py
+++ find_undefined_genres.py
@@ -1,13 +1,14 @@
 import glob
 
 from colorama import just_fix_windows_console
+from mutagen.aiff import AIFF
 from mutagen.easyid3 import EasyID3
 from mutagen.flac import FLAC
 from mutagen.mp4 import MP4
 from mutagen.oggvorbis import OggVorbis
 from mutagen.wave import WAVE
 
-FILETYPES = set(['flac', 'm4a', 'mp3', 'ogg', 'wav'])
+FILETYPES = sorted({'aif', 'aiff', 'flac', 'm4a', 'mp3', 'ogg', 'wav'})
 GENRES = set(
     [genre.strip() for genre in
      'Future House, Modern House, Dubstep, House, Progressive, Colour House'
@@ -16,30 +17,35 @@ GENRES = set(
 
 
 def check_filetype(files):
+
     for filepath in files:
+        match filetype:
+            case ftype if ftype in ['aif', 'aiff']:
+                mutagen_type = AIFF
+                genre_fieldname = 'TCON'
+            case 'flac':
+                mutagen_type = FLAC
+                genre_fieldname = 'genre'
+            case 'm4a':
+                mutagen_type = MP4
+                genre_fieldname = '©gen'
+            case 'mp3':
+                mutagen_type = EasyID3
+                genre_fieldname = 'genre'
+            case 'ogg':
+                mutagen_type = OggVorbis
+                genre_fieldname = 'genre'
+            case 'wav':
+                mutagen_type = WAVE
+                genre_fieldname = 'TCON'
+            case _:
+                print(f'\033[93mUndefined filetype \"{filetype}\"\033[0m\n')
+                return
 
         try:
-            match filetype:
-                case 'flac':
-                    audio = FLAC(filepath)
-                    genre_fieldname = 'genre'
-                case 'mp3':
-                    audio = EasyID3(filepath)
-                    genre_fieldname = 'genre'
-                case 'm4a':
-                    audio = MP4(filepath)
-                    genre_fieldname = '©gen'
-                case 'ogg':
-                    audio = OggVorbis(filepath)
-                    genre_fieldname = 'genre'
-                case 'wav':
-                    audio = WAVE(filepath)
-                    genre_fieldname = 'TCON'
-                case _:
-                    print(f'\033[93mUndefined filetype \"{filetype}\"\033[0m\n')
-                    return
+            audio = mutagen_type(filepath)
 
-            if (genre_fieldname in audio.keys()):
+            if genre_fieldname in audio.keys():
                 genre_fields = audio[genre_fieldname]
 
                 # join duplicate fields (Will not work with m4a files)
@@ -50,11 +56,12 @@ def check_filetype(files):
 
                 undefined_genres = list(set(genres) - GENRES)
 
-                if (len(undefined_genres) != 0):
+                if len(undefined_genres) != 0:
                     print(filepath)
                     print(f'Undefined Genres:{undefined_genres}\n')
+
         except Exception as e:
-            print(e)
+            print(f'{str(e)}\n')
 
 
 if __name__ == '__main__':
